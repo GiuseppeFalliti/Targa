2c288337684c1c21425c565d24ad09d65c1f1add
GiuseppeFalliti <isfuari66@gmail.com>
2023-10-31 10:16:48 +0100

    modifica  regex

--- Ip.java
+++ Ip.java
@@ -39,7 +39,7 @@ public class Ip {
     }
 
     public static void ip(String address) {
-        String ipValidate = ("192.168.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d.25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)");
+        String ipValidate = "192.168.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d).(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]\\d|\\d)";
         if (Pattern.matches(ipValidate, address)) {
             System.out.println(true);
         } else {
